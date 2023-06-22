967bb04f4ea9ecbbdd52b92622121339aa6e7837
sanjanadhenge <dhengesanjana2001@gmail.com>
2023-06-22 15:26:56 +0530

    [sanjana] Add : User Registration

---
+++ User_Registration/Validation.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace User_Registration
+{
+    public class Validation
+    {
+        public static string NAME_REGX = "^[A-Z]{1}[a-z]{2,}$";
+        public string ValidateFirstName(string firstName)
+        {
+            Regex reg = new Regex(NAME_REGX);
+            if (reg.IsMatch(firstName))
+            {
+                return "valid";
+            }
+            else
+            {
+                return "Invalid";
+            }
+
+        }
+    }
+}
